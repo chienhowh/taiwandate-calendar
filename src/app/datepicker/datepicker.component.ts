a7bf3c92c9362a8fe1cf434af457ede06777ac2d
chienhowh <63964025+chienhowh@users.noreply.github.com>
2021-05-13 14:34:31 +0800

    日歷頭尾加入

--- src/app/datepicker/datepicker.component.ts
+++ src/app/datepicker/datepicker.component.ts
@@ -10,18 +10,25 @@ import { Observable, timer } from 'rxjs';
 export class DatepickerComponent implements OnInit {
   @ViewChild('date_picker') date_picker: ElementRef;
   @ViewChild('dates') dates: ElementRef;
-  // 今天日期 for moment 運算
+
+
+  /** 今天日期 for moment 運算 */
   today = moment();
 
-  // header date: 預設今天
-  headerDate: Date = new Date();
+  /** 日 */
+  days = [];
 
-  timer;
-  // 顯示日期
+  /** header date: 預設今天 */
+  headerDate: Date = new Date();
+  headerDay: number;
+
+  /** 最後顯示日期 */
   selected_date = this.headerDate;
 
   // 中文週
   weekZh = ['日', 'ㄧ', '二', '三', '四', '五', '六'];
+
+
   constructor(
   ) { }
 
@@ -30,37 +37,80 @@ export class DatepickerComponent implements OnInit {
 
   }
 
+
+  // header start
   prevYear() {
-    this.today = this.today.subtract(1, 'y');
+    this.today.subtract(1, 'y');
     this.headerDate = this.headerShow(this.today.valueOf());
+    this.calendarShow();
 
   }
 
   prevMth() {
-    this.today = this.today.subtract(1, 'M');
+    this.today.subtract(1, 'M');
     this.headerDate = this.headerShow(this.today.valueOf());
+    this.calendarShow();
 
   }
   nextYear() {
-    this.today = this.today.add(1, 'y');
+    this.today.add(1, 'y');
     this.headerDate = this.headerShow(this.today.valueOf());
+    this.calendarShow();
 
   }
 
   nextMth() {
-    this.today = this.today.add(1, 'M');
+    this.today.add(1, 'M');
     this.headerDate = this.headerShow(this.today.valueOf());
+    this.calendarShow();
 
   }
+
+  /**translate to header date*/
+  headerShow(timestamp: number) {
+    return new Date(timestamp);
+  }
+  // header end
 
 
 
   toggleCalendar() {
-    this.dates.nativeElement.classList.toggle('active');
+    this.dates.nativeElement.classList.toggle('active');// 顯示日歷
+    this.headerDay = this.today.date();
+    console.log(this.today);
+
+    console.log(this.headerDay);
+
+    this.calendarShow();
+
   }
 
-  // translate to header date
-  headerShow(timestamp: number) {
-    return new Date(timestamp);
+  /** 月曆日期 */
+  calendarShow() {
+    const startDay = this.today.clone().startOf('month').day();
+
+    const days = this.today.daysInMonth(); // 當月天數
+    // days length 固定42
+    this.days = [];
+    // 加入前一個月的底
+    for (let i = 0; i < startDay; i++) {
+      this.days.push(i + 1);
+    }
+    // 當月日期
+    for (let i = 0; i < days; i++) {
+      this.days.push(i + 1);
+    }
+    // 加入後一個月的頭
+    const lengthLeft = this.days.length;
+    for (let i = 0; i < 42 - lengthLeft; i++) {
+      this.days.push(i + 1)
+    }
+
   }
+
+  selectDate(event) {
+    const date = +event.target.innerHTML;
+    this.headerDay = date;
+  }
+
 }
